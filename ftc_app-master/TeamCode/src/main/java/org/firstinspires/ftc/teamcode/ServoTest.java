f325c4f9491ddba75c02a05e3c0e514a7ef6fffa
Zain Asif <zain-asif@live.com>
2017-09-30 16:00:12 -0700

    Initial commit

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+
+@TeleOp(name="servotest")
+
+public class ServoTest extends LinearOpMode
+{
+
+    Servo servo = null;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        servo = hardwareMap.servo.get("servo1");
+
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+            telemetry.update();
+
+            servo.setPosition(0);
+
+            telemetry.addData("Servo Position", servo.getPosition());
+
+            sleep(5000);
+
+            servo.setPosition(16);
+            sleep(5000);
+
+            telemetry.addData("Servo Position", servo.getPosition());
+
+            idle();
+        }
+
+    }
+}
