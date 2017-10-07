ecada07d5444e26da1f1625ea8e4b4268c75ce6d
Chance Smith <smithcha7@stu.nthurston.k12.wa.us>
2017-10-07 14:19:12 -0700

    Added servo definition file

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servos.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/*
+ * Created by Chance- on 10/7/2017.
+ */
+
+public class Servos {
+
+    // Declaring motors
+    public Servo  servoArm  = null;
+
+    // Creating hwMap object and setting it to null
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    // Constructing Servos public object for use to call into another program
+    public Servos() {
+
+    }
+
+    // This section runs during initialization
+    public void init(HardwareMap ahwMap) {
+
+// Giving hwMap a value
+        hwMap = ahwMap;
+
+// Declaring servos to use in other classes
+        servoArm = hwMap.get(Servo.class, "servoArm");
+    }
+}
