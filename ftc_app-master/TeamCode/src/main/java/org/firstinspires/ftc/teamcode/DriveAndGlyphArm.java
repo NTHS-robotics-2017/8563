a387775dd67c36fba4b3d5526c3d572d1e4eab0b
Chance Smith <smicha02@gmail.com>
2017-10-31 17:01:55 -0700

    Add adjustable motor speeds to drive motors and arm motor

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveAndGlyphArm.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveAndGlyphArm.java
@@ -30,6 +30,8 @@ public class DriveAndGlyphArm extends LinearOpMode {
 
 // Defines variables
         double motorSpeed = 1;
+        boolean motorFast = true;
+        boolean armFast = true;
 
 // Sets motors to run without encoders for driver operation
         motors.left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -44,15 +46,41 @@ public class DriveAndGlyphArm extends LinearOpMode {
             telemetry.update();
             telemetry.addData("Servo Position: ", servos.claw.getPosition());
 
+// Defines if drive motor speed and arm motor speed are fast or slow
+            if (gamepad1.start && motorFast) {
+                motorFast = false;
+            } else if (gamepad1.start && !motorFast) {
+                motorFast = true;
+            }
+
+            if (gamepad1.back && armFast) {
+                armFast = false;
+            } else if (gamepad1.back && !armFast) {
+                armFast = true;
+            }
+
 // Sets drive motors to joystick locations
-            motors.left.setPower(gamepad1.left_stick_y);
-            motors.right.setPower(gamepad1.right_stick_y);
+            if (motorFast) {
+                motors.left.setPower(gamepad1.left_stick_y);
+                motors.right.setPower(gamepad1.right_stick_y);
+            } else {
+                motors.left.setPower(gamepad1.left_stick_y/2);
+                motors.right.setPower(gamepad1.right_stick_y/2);
+            }
 
 // Sets arm motor to motorSpeed or 0
             if (gamepad1.left_bumper) {
-                motors.arm.setPower(motorSpeed);
+                if (armFast) {
+                    motors.arm.setPower(motorSpeed);
+                } else {
+                    motors.arm.setPower(motorSpeed/2);
+                }
             } else if (gamepad1.right_bumper) {
-                motors.arm.setPower(-motorSpeed);
+                if (armFast) {
+                    motors.arm.setPower(-motorSpeed);
+                } else {
+                    motors.arm.setPower(-motorSpeed/2);
+                }
             } else {
                 motors.arm.setPower(0);
             }
@@ -62,7 +90,7 @@ public class DriveAndGlyphArm extends LinearOpMode {
                 servos.claw.setPosition(1);
                 Thread.sleep(250);
             } else if (gamepad1.left_trigger == 1) {
-                servos.claw.setPosition(.5);
+                servos.claw.setPosition(.4);
                 Thread.sleep(250);
             }
             idle();
