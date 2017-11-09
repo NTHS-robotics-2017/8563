143c3285aa044b62ca1ad3fb99196e0b7e0177e7
Chance Smith <smicha02@gmail.com>
2017-11-09 15:37:09 -0800

    Change arm motor to position of left joystick of gamepad 2 Add program for new claw (currently disabled)

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOpTwoController.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOpTwoController.java
@@ -1,6 +1,7 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -9,8 +10,9 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.Hardware.DriveMotors;
 import org.firstinspires.ftc.teamcode.Hardware.Servos;
 
-@TeleOp(name="DriveAndGlyphArmTwoController")
-public class DriveAndGlyphArmTwoController extends LinearOpMode {
+@Disabled
+@TeleOp(name="MainTeleOpTwoController")
+public class MainTeleOpTwoController extends LinearOpMode {
 
     // Declares robot object to get information from DriveMotors.java, Servos.java
     DriveMotors motors   = new DriveMotors();
@@ -32,18 +34,22 @@ public class DriveAndGlyphArmTwoController extends LinearOpMode {
 // Defines variables
         boolean motorFast = true;
         boolean armFast = true;
-        boolean buttonARelease = true;
         boolean leftBumperRelease = true;
-        boolean buttonAActive;
         boolean leftBumperActive;
         double currentRuntime;
-        double endRuntimeA = 0;
         double endRuntimeBumper = 0;
+
+// Claw servo positions
+        double leftClawOpen = 1;
+        double rightClawOpen = 0;
+        double leftClawClose = 0;
+        double rightClawClose = 1;
 
 // Sets motors to run without encoders for driver operation
         motors.left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motors.right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        servos.claw.setPosition(1);
+        servos.clawLeft.setPosition(leftClawOpen);
+        servos.clawRight.setPosition(rightClawOpen);
 
         waitForStart();
 
@@ -54,26 +60,21 @@ public class DriveAndGlyphArmTwoController extends LinearOpMode {
 
 // Updates telemetry data
             telemetry.update();
-            telemetry.addData("Servo Position: ", servos.claw.getPosition());
+            telemetry.addData("Left Claw Servo Position: ", servos.leftclaw.getPosition());
+            telemetry.addData("Right Claw Servo Position: ", servos.rightClaw.getPosition());
             telemetry.addData("Motor Fast: ", motorFast);
             telemetry.addData("Arm Fast: ", armFast);
             telemetry.addData("Current Runtime: ", currentRuntime);
-            telemetry.addData("End Runtime Button A: ", endRuntimeA);
             telemetry.addData("End Runtime Left Bumper: ", endRuntimeBumper);
 
-// Sets button and trigger to active if it has been 2 seconds
-            if (currentRuntime >= endRuntimeA) {
-                buttonAActive = true;
-            } else {
-                buttonAActive = false;
-            }
+// Sets button and trigger to active if it has been 1 seconds
             if (currentRuntime >= endRuntimeBumper) {
                 leftBumperActive = true;
             } else {
                 leftBumperActive = false;
             }
 
-// Defines if drive motor speed and arm motor speed are fast or slow
+// Defines if drive motor speed speed are fast or slow
             if (gamepad1.left_bumper && leftBumperActive) {
                 if (leftBumperRelease) {
                     motorFast = !motorFast;
@@ -81,14 +82,7 @@ public class DriveAndGlyphArmTwoController extends LinearOpMode {
                 }
             }
 
-            if (gamepad2.a && buttonAActive) {
-                if (buttonARelease) {
-                    armFast = !armFast;
-                    endRuntimeA = currentRuntime+1000;
-                }
-            }
-
-// Sets drive motors to joystick locations
+// Sets drive motors to joystick position of gamepad 1
             if (motorFast) {
                 motors.left.setPower(gamepad1.left_stick_y);
                 motors.right.setPower(gamepad1.right_stick_y);
@@ -97,15 +91,19 @@ public class DriveAndGlyphArmTwoController extends LinearOpMode {
                 motors.right.setPower(gamepad1.right_stick_y/4);
             }
 
-// Sets arm motor to joystick position of gamepad 2
+// Sets arm motor to left joystick position of gamepad 2
             motors.arm.setPower(gamepad2.left_stick_y);
 
 // Sets claw servo to open or closed position
             if (gamepad2.right_trigger == 1) {
-                servos.claw.setPosition(1);
+                servos.leftClaw.setPosition(leftClawOpen);
+                servos.rightClaw.setPosition(rightClawOpen);
             } else if (gamepad2.left_trigger == 1) {
-                servos.claw.setPosition(.4);
+                servos.leftClaw.setPosition(leftClawClose);
+                servos.rightClaw.setPosition(rightClawClose);
             }
+
+
             idle();
         }
     }
